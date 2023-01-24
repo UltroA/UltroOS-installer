52abf18877ac71f7248a8361b7940884de4a89e6
UltroA <64162002+UltroA@users.noreply.github.com>
2023-01-25 00:58:30 +0300

    deleted disk encription and added disk selection function

--- main.py
+++ main.py
@@ -1,44 +1,53 @@
 import archinstall
 import getpass
+import os
 
-# Select a harddrive and a disk password
-harddrive = archinstall.select_disk(archinstall.all_blockdevices(partitions=False))
-disk_password = getpass.getpass(prompt='Disk password (won\'t echo): ')
 
-# We disable safety precautions in the library that protects the partitions
-harddrive.keep_partitions = False
+class Installer:
+    def __init__(self):
+        self.boot = ''
+        self.root = ''
+        self.harddrive = ''
 
-# First, we configure the basic filesystem layout
-with archinstall.Filesystem(harddrive, archinstall.GPT) as fs:
-    # We create a filesystem layout that will use the entire drive
-    # (this is a helper function, you can partition manually as well)
-    fs.use_entire_disk(root_filesystem_type='btrfs')
+    def diskwork(self):
+        # Select a harddrive and a disk password
+        os.system("lsblk")
+        diskn = input('Enter disk name in format: "sdX or nvmeXnY" (where X and Y is nums): ')
+        self.harddrive = archinstall.select_disk(archinstall.select_disk(diskn))
 
-    boot = fs.find_partition('/boot')
-    root = fs.find_partition('/')
+        # We disable safety precautions in the library that protects the partitions
+        self.harddrive.keep_partitions = False
 
-    boot.format('vfat')
+        # First, we configure the basic filesystem layout
+        with archinstall.Filesystem(self.harddrive, archinstall.GPT) as fs:
+            # We create a filesystem layout that will use the entire drive
+            # (this is a helper function, you can partition manually as well)
+            fs.use_entire_disk(root_filesystem_type='btrfs')
 
-    # Set the flag for encrypted to allow for encryption and then encrypt
-    root.encrypted = True
-    root.encrypt(password=disk_password)
+            self.boot = fs.find_partition('/boot')
+            self.root = fs.find_partition('/')
 
-with archinstall.luks2(root, 'luksloop', disk_password) as unlocked_root:
-    unlocked_root.format(root.filesystem)
-    unlocked_root.mount('/mnt')
+            self.boot.format('vfat')
 
-    boot.mount('/mnt/boot')
+            self.boot.mount('/mnt/boot')
 
-with archinstall.Installer('/mnt') as installation:
-    if installation.minimal_installation(hostname='UltroOS'):
-        installation.add_bootloader()
+    def install_needed_and_mount(self):
+        with archinstall.Installer('/mnt') as installation:
+            if installation.minimal_installation(hostname='UltroOS'):
+                installation.add_bootloader()
 
-        installation.add_additional_packages(['vim', 'wget', 'git'])
+                installation.add_additional_packages(['vim', 'wget', 'git'])
 
-        # Optionally, install a profile of choice.
-        # In this case, we install a minimal profile that is empty
-        installation.install_profile('gnome')
+                # Optionally, install a profile of choice.
+                # In this case, we install a minimal profile that is empty
+                installation.install_profile('gnome')
 
-        user = User('devel', 'devel', False)
-        installation.create_users(user)
-        installation.user_set_pw('root', 'airoot')
+                user = User('devel', 'devel', False)
+                installation.create_users(user)
+                installation.user_set_pw('root', 'airoot')
+
+
+if __name__ == '__main__':
+    inst = Installer()
+    inst.diskwork()
+    inst.install_needed_and_mount()
